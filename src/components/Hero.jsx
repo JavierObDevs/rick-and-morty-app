df5add398c16ae1be24fac3f69728550a5f68695
JavvDevv <javier.alexander.obb@gmail.com>
2024-03-14 17:06:19 -0500

    Add atropos link rel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,66 +1,54 @@
 import "./Hero.css";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import UseParallax from '../hooks/UseParallax';
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { useRef } from "react";
+import Atropos from 'atropos/react';
 
-gsap.registerPlugin(ScrollTrigger);
+
 const Hero = () => {
-  
-
-  const box = useRef();
-
-  useGSAP(() => {
-    gsap.to(".bg", {
-      ScrollTrigger: {
-        scrub: 3,
-      },
-      scale: 1.1,
-    });
-
-    gsap.to(".clouds-1", {
-      ScrollTrigger: {
-        scrub: 3,
-      },
-      x: 300,
-    });
-  }, { scope: box});
 
   return (
     <main>
-      <section ref={box}>
+      <Atropos className="my-atropos">
+        
         <img
           src="images\bg-z10.png"
           alt="image background"
           id="bg"
           className="bg"
+          data-atropos-offset="0"
         />
+
         <img
           src="images\text-z9.webp"
           alt="Text Rick And Morty"
           id="text"
           className="text"
+          data-atropos-offset="10"
         />
+
         <img
           src="images\pj-z8.png"
           alt="Rick and Morty PNG"
           id="pjs"
           className="pjs"
+          data-atropos-offset="3"
         />
+
         <img
           src="images\clouds-1.png"
           alt="Clouds"
           className="clouds-1"
           id="clouds_2"
+          data-atropos-offset="-5"
         />
+
         <img
           src="images\clouds-2.png"
           alt="Clouds"
           className="clouds-2"
           id="clouds_1"
+          data-atropos-offset="5"
         />
-      </section>
+        
+      </Atropos>
     </main>
   );
 };
